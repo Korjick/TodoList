581466403ff749c76d0ae4843f2345b8b86531cf
Bulat Din <zzgriim@gmail.com>
2023-03-07 20:11:25 +0300

    Add Room

--- app/src/main/java/ru/itlab/todolist/Note.java
+++ app/src/main/java/ru/itlab/todolist/Note.java
@@ -1,10 +1,17 @@
 package ru.itlab.todolist;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
     public int priority;
     public String text;
 
-    public Note(int priority, String text) {
+    public Note(int id, int priority, String text) {
+        this.id = id;
         this.priority = priority;
         this.text = text;
     }
