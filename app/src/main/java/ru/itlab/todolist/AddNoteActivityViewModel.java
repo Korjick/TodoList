d253b8f89c9ddb094fbd275c950dab080ea977ae
Bulat Din <zzgriim@gmail.com>
2023-03-17 20:05:15 +0300

    Final

--- app/src/main/java/ru/itlab/todolist/AddNoteActivityViewModel.java
+++ app/src/main/java/ru/itlab/todolist/AddNoteActivityViewModel.java
@@ -7,14 +7,28 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
 public class AddNoteActivityViewModel extends AndroidViewModel {
     private NotesDatabase notesDatabase;
     private MutableLiveData<Boolean> addNoteLiveData;
+    private CompositeDisposable compositeDisposable;
 
     public AddNoteActivityViewModel(@NonNull Application application) {
         super(application);
         notesDatabase = NotesDatabase.getInstance(application);
         addNoteLiveData = new MutableLiveData<>(false);
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 
     public LiveData<Boolean> getAddNoteLiveData(){
@@ -22,9 +36,18 @@ public class AddNoteActivityViewModel extends AndroidViewModel {
     }
 
     public void add(Note note){
-        new Thread(() -> {
-            notesDatabase.notesDAO().add(note);
-            addNoteLiveData.postValue(true);
-        }).start();
+        Disposable disposable = notesDatabase
+                .notesDAO()
+                .add(note)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        addNoteLiveData.setValue(true);
+                    }
+                });
+
+        compositeDisposable.add(disposable);
     }
 }
